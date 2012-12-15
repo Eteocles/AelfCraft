3b2794d93bd5d0667e6259655c17a607000d0887
JT <jonathan.t.cho@gmail.com>
2012-12-15 02:13:13 -0800

    Updated some Alf stuff.

--- ALFCore/src/com/alf/chararacter/Alf.java
+++ ALFCore/src/com/alf/chararacter/Alf.java
@@ -1,5 +1,24 @@
 package com.alf.chararacter;
 
+import org.bukkit.entity.LivingEntity;
+
+import com.alf.AlfCore;
+
+/**
+ * Encapsulates a player type.
+ * @author Eteocles
+ *
+ */
 public class Alf extends CharacterTemplate {
+
+	public Alf(AlfCore plugin, LivingEntity lEntity, String name) {
+		super(plugin, lEntity, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getMaxHealth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
