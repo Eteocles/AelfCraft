85ee5e781b17dd167f2142ef86acb30a527a595c
JT <jonathan.t.cho@gmail.com>
2012-12-20 16:44:04 -0800

    Updating more of YMLAlfStorage for reference.

--- ALFCore/src/com/alf/listener/APlayerListener.java
+++ ALFCore/src/com/alf/listener/APlayerListener.java
@@ -2,17 +2,23 @@ package com.alf.listener;
 
 import net.minecraft.server.EntityPlayer;
 
+import org.bukkit.ChatColor;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 import com.alf.AlfCore;
 import com.alf.chararacter.Alf;
+import com.alf.chararacter.CharacterManager;
+import com.alf.chararacter.effect.CombatEffect;
+import com.alf.command.Command;
 
 /**
  * Handle player related events.
@@ -28,6 +34,63 @@ public class APlayerListener implements Listener {
 	 */
 	public APlayerListener(AlfCore plugin) {
 		this.plugin = plugin;
+	}
+	
+	/**
+	 * Handle player joining.
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		CharacterManager cm = this.plugin.getCharacterManager();
+		final Alf alf = cm.getAlf(player);
+		
+		/**
+		 * Forcefully reset combat effect.
+		 */
+		if (! alf.hasEffect("Combat")) {
+			alf.resetCombatEffect();
+		}
+		
+//		cm.checkClass(alf);
+//		alf.syncExperience();
+//		alf.syncHealth();
+//		alf.checkInventory();
+		
+		//class prefix name
+		
+		//bonus expiration
+		
+		this.plugin.getServer().getScheduler().scheduleSyncDelayedTask(this.plugin, new Runnable() {
+			public void run() {
+//				APlayerListener.this.plugin.getCharacterManager().performSkillChecks(alf);
+//				alf.checkInventory();
+			}
+		}, 5L);
+		player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Welcome to AelfCraft!");
+	}
+	
+	/**
+	 * Handle player quitting.
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		Player player = event.getPlayer();
+		CharacterManager alfManager = this.plugin.getCharacterManager();
+		Alf alf = alfManager.getAlf(player);
+		alf.cancelDelayedSkill();
+		alf.clearEffects();
+		if (alf.isInCombat())
+			alf.leaveCombat(CombatEffect.LeaveCombatReason.LOGOUT);
+		
+		alfManager.saveAlf(alf, true);
+		alfManager.removeAlf(alf);
+		
+		for (Command command : this.plugin.getCommandHandler().getCommands())
+			if (command.isInteractive())
+				command.cancelInteraction(player);
 	}
 	
 	/**
--- ALFCore/src/com/alf/persistence/YMLAlfStorage.java
+++ ALFCore/src/com/alf/persistence/YMLAlfStorage.java
@@ -1,32 +1,148 @@
 package com.alf.persistence;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 import com.alf.AlfCore;
 import com.alf.chararacter.Alf;
 
+/**
+ * Stores Alf data in YML.
+ * @author Eteocles
+ */
 public class YMLAlfStorage extends AlfStorage {
 
+	private final File playerFolder;
+	//Queue for Alfs to save.
+	private Map<String, Alf> toSave = new ConcurrentHashMap<String, Alf>();
+	private final int SAVE_INTERVAL = 6000;
+	private int id = 0;
+
+	/**
+	 * Constructs the YML storage.
+	 * @param plugin
+	 */
 	public YMLAlfStorage(AlfCore plugin) {
 		super(plugin);
+		this.playerFolder = new File(plugin.getDataFolder(), "players");
+		this.playerFolder.mkdirs();
+
+		this.id = Bukkit.getScheduler().scheduleAsyncRepeatingTask(plugin, 
+				new AlfSaveThread(), SAVE_INTERVAL, SAVE_INTERVAL);
 	}
 
-	@Override
 	public Alf loadAlf(Player player) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
+	/**
+	 * Save the given Alf, either later or instantly.
+	 */
 	public void saveAlf(Alf alf, boolean now) {
-		// TODO Auto-generated method stub
-		
+		if (now)
+			try {
+				doSave(alf);
+			} catch (IOException e) {
+				AlfCore.log(Level.SEVERE, "There was a problem saving the Alf: " + alf.getName());
+				this.toSave.put(alf.getName(), alf);
+			}
+		else {
+			this.toSave.put(alf.getName(), alf);
+		}
 	}
 
-	@Override
+	/**
+	 * Save all Alf values.
+	 * @param alf
+	 * @throws IOException
+	 */
+	public boolean doSave(Alf alf) throws IOException {
+		String name = alf.getName();
+		File playerFile = new File(this.playerFolder+File.separator+name.substring(0, 1).toLowerCase(), 
+				name + ".yml");
+		FileConfiguration playerConfig = new YamlConfiguration();
+		
+		//Save Alf class.
+		playerConfig.set("class", alf.getAlfClass().toString());
+		//Save second class.
+		if (alf.getSecondClass() != null) {
+			playerConfig.set("secondary-class", alf.getSecondClass().toString());
+		}
+		//
+		playerConfig.set("verbose", alf.isVerbose());
+//		playerConfig.set("suppressed", new ArrayList<String>(alf.getSuppressedSkills()));
+		playerConfig.set("mana", alf.getMana());
+		playerConfig.set("health", alf.getHealth());
+		
+		saveSkillSettings(alf, playerConfig.createSection("skill-settings"));
+//		saveCooldowns(alf, playerConfig.createSection("cooldowns"));
+		saveExperience(alf, playerConfig.createSection("experience"));
+		saveBinds(alf, playerConfig.createSection("binds"));
+		
+		playerConfig.save(playerFile);
+		
+		return true;
+	}
+	
+	private void saveSkillSettings(Alf alf, ConfigurationSection config) {
+		
+	}
+	
+	private void saveExperience(Alf alf, ConfigurationSection config) {
+		
+	}
+	
+	private void saveBinds(Alf alf, ConfigurationSection config) {
+		
+	}
+	
+
 	public void shutdown() {
 		// TODO Auto-generated method stub
-		
+
+	}
+
+	/**
+	 * Handles timed, repeated saving of Alfs.
+	 * @author Eteocles
+	 */
+	protected class AlfSaveThread implements Runnable {
+
+		protected AlfSaveThread() {}
+
+		/**
+		 * Repeated function of save thread. 
+		 */
+		public void run() {
+			if (! YMLAlfStorage.this.toSave.isEmpty()) {
+				Iterator<Entry<String, Alf>> iter = YMLAlfStorage.this.toSave.entrySet().iterator();
+				//Iterate through all of the Alfs to be saved.
+				while (iter.hasNext()) {
+					Alf alf = (Alf) ((Map.Entry<String, Alf>)iter.next()).getValue();
+					try {
+						YMLAlfStorage.this.doSave(alf);
+					} catch (Exception e) {
+						AlfCore.log(Level.SEVERE, "There was a problem saving the Alf: " + alf.getName());
+						e.printStackTrace();
+					}
+					//Remove the current iterative element before advancing.
+					iter.remove();
+				}
+			}
+		}
 	}
 
 }
